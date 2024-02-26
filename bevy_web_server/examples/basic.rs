40275109935a0ad8d5c9cd437f8e5e26d7e5e21e
Søren Alsbjerg Hørup <sah@sah.dk>
2024-02-26 14:49:35 +0100

    added working example

--- bevy_web_server/examples/basic.rs
+++ bevy_web_server/examples/basic.rs
@@ -7,30 +7,72 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Serialize, Deserialize)]
 enum Msg {
-    Ping(u64),
-    Pong(u64),
+    Ping(u128),
+    Pong(u128),
 }
 
-fn server_system(connections:Query<&Connection>, mut writer:EventWriter<bevy_web_server::SendPacket<Msg>>) {
+fn server_system(
+    connections: Query<&Connection>,
+    mut writer: EventWriter<bevy_web_server::SendPacket<Msg>>,
+    time: Res<Time>,
+    mut reader: EventReader<bevy_web_server::RecvPacket<Msg>>,
+) {
     for conn in connections.iter() {
         writer.send(SendPacket {
-            connection_id:conn.id.clone(),
-            msg:Msg::Ping(0)
+            connection_id: conn.id.clone(),
+            msg: Msg::Ping(time.elapsed().as_millis()),
         });
+    }
+
+    for packet in reader.read() {
+        match &packet.msg {
+            Msg::Ping(_) => {}
+            Msg::Pong(ms) => {
+                let current_ms = time.elapsed().as_millis();
+                let diff = current_ms - ms.to_owned();
+                println!("{} ping: {}ms", packet.connection, diff);
+            }
+        }
     }
 }
 
-fn client_system() {
+fn client_system(
+    mut reader: EventReader<bevy_web_client::RecvPacket<Msg>>,
+    mut writer: EventWriter<bevy_web_client::SendPacket<Msg>>,
+) {
+    for packet in reader.read() {
+        match &packet.msg {
+            Msg::Ping(ms) => {
+                writer.send(bevy_web_client::SendPacket {
+                    msg: Msg::Pong(ms.to_owned()),
+                });
+            }
+            Msg::Pong(_) => {}
+        }
+    }
 }
 
 fn main() {
-    // Start an App that acts both as a Server and a Client.
-    // i.e. the app connects to itself.
+    let server_tick_rate = 1.0 / 64.0; 
+    let client_tick_rate = 0.0; // unlimited;
+    let server_thread = std::thread::spawn(move || {
+        App::new()
+            .add_plugins(BevyWebServerPlugin::new() as BevyWebServerPlugin<Msg>)
+            .add_plugins(MinimalPlugins.set(ScheduleRunnerPlugin::run_loop(
+                Duration::from_secs_f32(server_tick_rate),
+            )))
+            .add_systems(Update, server_system)
+            .run();
+    });
     App::new()
-        .add_plugins(BevyWebServerPlugin::new() as BevyWebServerPlugin<Msg>)
         .add_plugins(BevyWebClientPlugin::new() as BevyWebClientPlugin<Msg>)
-        .add_plugins(MinimalPlugins.set(ScheduleRunnerPlugin::run_loop(Duration::from_millis(100))))
-        .add_systems(Update, server_system)
+        .add_plugins(
+            MinimalPlugins.set(ScheduleRunnerPlugin::run_loop(Duration::from_secs_f32(
+                client_tick_rate,
+            ))),
+        )
         .add_systems(Update, client_system)
         .run();
+
+    server_thread.join().expect("failed to join");
 }
